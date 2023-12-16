6819d7e857be485e421ee65f7e8e5bc526ca90e1
farouqdaif1 <daiffarouq@gmail.com>
2023-12-16 16:36:19 +0200

    feat(structure): add initial project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Home from "./components/Home/Home"
+import Navbar from "./components/NavBar/Navbar"
 function App() {
   return (
     <>
-      <h1>React App</h1>
+      <Navbar />
+      <Home />
     </>
   )
 }
