2201ee1b7a22b0981f00b52ce8346d60960ff73b
farouqdaif1 <daiffarouq@gmail.com>
2023-12-17 21:25:08 +0200

    style(switcher): fix switch styles

--- src/components/NavBar/SwitchLang/SwitchLang.jsx
+++ src/components/NavBar/SwitchLang/SwitchLang.jsx
@@ -6,12 +6,17 @@ const SwitchLang = () => {
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
     document.documentElement.dir = i18n.dir(lng);
-
   };
+  const { t } = useTranslation();
+
   return (
-    <div>
-      <button onClick={() => changeLanguage("en")}>English</button>
-      <button onClick={() => changeLanguage("ar")}>Arabic</button>
+    <div className="language-switcher">
+      <button className="language-button" onClick={() => changeLanguage("en")}>
+       {t('English')}
+      </button>
+      <button className="language-button" onClick={() => changeLanguage("ar")}>
+        {t('Arabic')}
+      </button>
     </div>
   );
 };
